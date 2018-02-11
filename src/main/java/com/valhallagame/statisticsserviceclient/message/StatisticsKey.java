9c8dd12e66be4e666ff3c8b0c61f8a4882ac35fc
Mikael Berglund <mikbe@kth.se>
2018-02-11 13:19:24 +0100

    Add same keys as in game

--- src/main/java/com/valhallagame/statisticsserviceclient/message/StatisticsKey.java
+++ src/main/java/com/valhallagame/statisticsserviceclient/message/StatisticsKey.java
@@ -1,5 +1,21 @@
 package com.valhallagame.statisticsserviceclient.message;
 
 public enum StatisticsKey {
-	EINHARJER_KILLED, TRAINING_GROUNDS_CLEAR_TIME
+	/*----------------- ENEMIES --------------------*/
+	PLAYER_CHARACTER_KILLED,
+	
+	FREDRIC_KILLED,
+	EINHARJER_KILLED,
+	BERSERKER_KILLED,
+	SCOUT_KILLED,
+	MAGE_KILLED,
+	CHIEFTAIN_KILLED,
+
+	/*----------------- BOSSES --------------------*/
+	GRYNMA_KILLED,
+
+	/*------------ CHARACTER RELATED ---------------*/
+	DEATHS,
+	DAMAGE_DONE,
+	DAMAGE_TAKEN
 }
