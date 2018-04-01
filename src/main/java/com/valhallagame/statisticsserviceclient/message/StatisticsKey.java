5db853db17890bc888adce547d0326739a824b03
Mikael Berglund <mikbe@kth.se>
2018-04-02 00:17:36 +0200

    Synced with unreal

--- src/main/java/com/valhallagame/statisticsserviceclient/message/StatisticsKey.java
+++ src/main/java/com/valhallagame/statisticsserviceclient/message/StatisticsKey.java
@@ -4,12 +4,17 @@ public enum StatisticsKey {
 	/*----------------- ENEMIES --------------------*/
 	PLAYER_CHARACTER_KILLED,
 	
-	FREDRIC_KILLED,
 	EINHARJER_KILLED,
 	BERSERKER_KILLED,
 	SCOUT_KILLED,
 	MAGE_KILLED,
+	LOOTER_KILLED,
+	GUARD_KILLED,
+	QUARTERMASTER_KILLED,
 	CHIEFTAIN_KILLED,
+
+	/*----------------- MISUNDERSTOOD ENEMIES --------------------*/
+	FREDRIC_KILLED,
 
 	/*----------------- BOSSES --------------------*/
 	GRYNMA_KILLED,
@@ -17,7 +22,7 @@ public enum StatisticsKey {
 	/*------------ CHARACTER RELATED ---------------*/
 	DEATHS,
 	DAMAGE_DONE,
-	DAMAGE_TAKEN, 
+	DAMAGE_TAKEN,
 	
 	/*------------ Dungeon RELATED ---------------*/
 	TRAINING_GROUNDS_CLEAR_TIME //Should this be in its own enum?
